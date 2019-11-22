ecd09ad79131618473dc175a3a1084be2e6d612f
Ajay <Ajay@AJAYLAPTOP>
2019-11-22 11:40:11 -0800

    Add project files.

---
+++ BPA Game/Item.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BPA_Game
+{
+
+    enum rarity { common, uncommon, rare, epic, legendary }; //used for saying how rare this item is compared to other items
+    class Item
+    {
+        //private variables
+        private rarity _itemRarity; //the rarirty of the item
+        
+        public Item(rarity itemRarity)
+        {
+            _itemRarity = itemRarity;
+
+        }
+    }
+}
