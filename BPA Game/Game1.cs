3679b53bf1eda1db6c75061ddd415cc06eb65bdb
BuildTools <unconfigured@null.spigotmc.org>
2019-11-23 13:33:56 -0800

    -Cleaned up the comments

--- BPA Game/Game1.cs
+++ BPA Game/Game1.cs
@@ -4,9 +4,7 @@ using Microsoft.Xna.Framework.Input;
 
 namespace BPA_Game
 {
-    /// <summary>
-    /// This is the main type for your game.
-    /// </summary>
+
     public class Game1 : Game
     {
         GraphicsDeviceManager graphics;
@@ -17,60 +15,48 @@ namespace BPA_Game
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
         }
-
-        /// <summary>
+       
         /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
+        /// This is where it can query for any required services and load any non-graphic related content.  
+        /// Calling base.Initialize will enumerate through any components and initialize them as well.
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+            // TODO: 
+            //Add initialization logic here
 
             base.Initialize();
         }
 
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
+        // LoadContent will be called once per game and is the place to load  all of your content.
         protected override void LoadContent()
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
+            // TODO: 
+            //use this.Content to load your game content here
         }
 
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// game-specific content.
-        /// </summary>
+        // UnloadContent will be called once per game and is the place to unload game-specific content.
         protected override void UnloadContent()
         {
-            // TODO: Unload any non ContentManager content here
+            // TODO: 
+            //Unload any non ContentManager content here
         }
 
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        // Allows the game to run logic such as updating the world, checking for collisions, gathering input, and playing audio.
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
+            // TODO: 
+            //Add  update logic here
 
             base.Update(gameTime);
         }
 
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        // This is called when the game should draw itself.
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
